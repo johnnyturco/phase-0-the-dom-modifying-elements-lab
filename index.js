210f5cfc6e742a4fd48fa69b75746fc95b911239
Johnny <johnnyturco@me.com>
2022-10-18 10:54:42 -0500

    commit tree 4f094a8691ddec65215241a66f90629783c52805

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Johnny Turco is the champion';
+document.body.append(newHeader);
+
+// newHeader.setAttribute('id', 'victory');
+
